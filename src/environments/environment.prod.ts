a6c13fd947c9a3f892623c88c0f1ef827a22082d
ClaudRO <claudiorodriguez7778@gmail.com>
2025-04-14 15:03:15 -0400

    detalle credenciales de conexion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:5000/api' // Ajusta el puerto si es necesario
+    apiUrl: 'https://backend-vimerzion-gtse.onrender.com/api'
+    /*apiUrl: 'http://localhost:5000/api' // Ajusta el puerto si es necesario*/
   };
