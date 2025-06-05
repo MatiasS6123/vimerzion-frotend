77ed89983474b892568da19a7ef7442d2c660ec9
ClaudRO <claudiorodriguez7778@gmail.com>
2025-06-05 00:40:27 -0400

    cambiodecuenta

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://backend-vimerzion-gtse.onrender.com/api'
-    /*apiUrl: 'http://localhost:5000/api' // Ajusta el puerto si es necesario*/
+    apiUrl: 'https://vimerzionclon.onrender.com/api',
+    //apiUrl: 'http://localhost:5000/api' // Ajusta el puerto si es necesario*/
   };
